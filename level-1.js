89cc6a077e866d2491cfa515243754a4f2f2c890
Rajesh  A <rajesha@us.megatrends.com>
2017-06-02 12:53:02 -0400

    Please enter the commit message for your changes. Lines starting

--- level-1.js
+++ level-1.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//---------------- CHALLENGE #1 -------------------
+//console.log("HELLO WORLD");
+
+//---------------- CHALLENGE #2 -------------------
+/*
+var sum = 0;
+for (var index = 2; index < process.argv.length; index++) {
+    sum += +process.argv[index];    
+}
+
+console.log(sum);
+*/
+
+//---------------- CHALLENGE #3 -------------------
