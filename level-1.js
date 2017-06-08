d969545e0e74e93130f8d0ab8706f8683d7a4511
Rajesh  A <rajesha@us.megatrends.com>
2017-06-08 18:00:40 -0400

    challenge 4 completed

--- level-1.js
+++ level-1.js
@@ -20,3 +20,13 @@ var len = fs.readFileSync(process.argv[2]).toString().split('\n').length - 1
 // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
 console.log(len);
 */
+
+//---------------- CHALLENGE #4 -------------------
+var fs = require('fs');
+
+var len = 0;
+
+fs.readFile(process.argv[2], 'utf8', (err,data) => {
+    console.log(data.split('\n').length - 1);
+});
+//console.log("async called");
